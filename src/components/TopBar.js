27995cf495cbbeab544a3a609f81c5b9d005d600
Patrick Chen <patrickchen1994@gmail.com>
2017-11-21 18:00:53 +0800

    Add content block 1

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
-import { Menu, Icon, Input, Container, Image } from 'semantic-ui-react'
+import { Menu, Icon, Input, Image } from 'semantic-ui-react'
+import '../containers/Container.css'
+
 import './TopBar.css'
 import ScrollHOC from './ScrollHOC'
 
 const SmallTopBar = ({ style }) => (
-    <div className='small-top-bar top-bar' style={style}>
+    <div className='small-top-bar top-bar my-container' style={style}>
         <span style={{ fontSize: 30 }}><b>NTUEE ESLab</b>
         </span>
         <Image src='assets/ntu_logo.white.png' className='ntu-logo' inline size='small'/>
@@ -15,22 +17,22 @@ const SmallTopBar = ({ style }) => (
 const BigTopBar = ({ className, style }) => (
     <div className={className} style={style}>
         <p style={{ paddingTop: 20 }}>
-            <span style={{ fontSize: 30 }}><b>嵌入式系統實驗</b></span>
+            <span style={{ fontSize: 40 }}><b>嵌入式系統實驗</b></span>
             <br />
             <span style={{ paddingLeft: 2 }}>國立台灣大學 電機工程學系</span>
         </p>
-        <Image src='assets/ntu_logo.white.png' className='ntu-logo' inline size='medium' verticalAlign='middle'/>
+        <Image src='assets/ntu_logo.white.png' className='ntu-logo' inline size='small' verticalAlign='middle'/>
     </div>
 )
 
 const _TopBar = ({ bigFont = true }) => {
     const className = bigFont ?
-        'big-top-bar top-bar' : 'big-hidden-top-bar top-bar'
+        'big-top-bar top-bar my-container' : 'big-hidden-top-bar top-bar my-container'
     return (
         <div>
             <SmallTopBar/>
             <BigTopBar className={className}/>
-            <BigTopBar className='big-top-bar top-bar'
+            <BigTopBar className='big-top-bar top-bar my-container'
                 style={{
                     visibility: 'hidden',
                     position: 'initial',
