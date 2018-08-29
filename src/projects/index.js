0f739cfc21eea7291dcca3a69d8d2a8530fb06eb
Ian Chen <r05921035@ntu.edu.tw>
2018-08-30 00:22:45 +0800

    Transition

---
+++ src/projects/index.js
@@ -0,0 +1,10 @@
+// eslint-disable-next-line
+import project2017fall from '!json-loader!yaml-loader!./2017fall.yaml'
+
+console.log(project2017fall)
+
+const projects = [
+    project2017fall,
+]
+
+export default projects
