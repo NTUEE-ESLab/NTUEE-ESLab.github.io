bcf5e82e8ed14944cdf6edb6ca35461abd025649
Anthony Chen <r05921042@ntu.edu.tw>
2019-01-21 22:24:38 +0800

    import project2018fall

--- src/projects/index.js
+++ src/projects/index.js
@@ -1,8 +1,10 @@
 // eslint-disable-next-line
 import project2017fall from '!json-loader!yaml-loader!./2017fall.yaml'
-
+// eslint-disable-next-line
+import project2018fall from '!json-loader!yaml-loader!./2018fall.yaml'
 const projects = [
     project2017fall,
+    project2018fall,
 ]
 
 export default projects
