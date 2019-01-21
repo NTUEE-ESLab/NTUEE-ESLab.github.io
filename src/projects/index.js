57e154717958802df4fc20fac25f1488cb04cd0a
Anthony Chen <r05921042@ntu.edu.tw>
2019-01-21 22:32:19 +0800

    adjust session order

--- src/projects/index.js
+++ src/projects/index.js
@@ -3,8 +3,8 @@ import project2017fall from '!json-loader!yaml-loader!./2017fall.yaml'
 // eslint-disable-next-line
 import project2018fall from '!json-loader!yaml-loader!./2018fall.yaml'
 const projects = [
-    project2017fall,
     project2018fall,
+    project2017fall,
 ]
 
 export default projects
