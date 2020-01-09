c760f4e74d832cebad2c45d978d859ecf1756c77
liu_014 <32957968+liu014@users.noreply.github.com>
2020-01-09 20:37:47 +0800

    Update index.js

--- src/projects/index.js
+++ src/projects/index.js
@@ -2,7 +2,10 @@
 import project2017fall from '!json-loader!yaml-loader!./2017fall.yaml'
 // eslint-disable-next-line
 import project2018fall from '!json-loader!yaml-loader!./2018fall.yaml'
+// eslint-disable-next-line
+import project2018fall from '!json-loader!yaml-loader!./2019fall.yaml'
 const projects = [
+    project2019fall,
     project2018fall,
     project2017fall,
 ]
